57c0ac3288c47ed1925afa8c1cdb787fcc2b2617
CommercialCrew <octsec0912@gmail.com>
2024-05-27 15:34:39 +0900

    초기 커밋.

---
+++ LAB001/LAB001.cpp
@@ -0,0 +1,118 @@
+﻿#include <windows.h>
+
+// 전역 변수 선언
+HINSTANCE hInst;
+LPCWSTR szWindowClass = L"win32app";
+LPCWSTR szTitle = L"ANU";
+
+// 함수 선언
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void DrawTriangle(HDC hdc);
+void DrawRectangle(HDC hdc);
+void DrawCircle(HDC hdc);
+
+// WinMain 함수
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow) {
+    WNDCLASSEX wcex;
+    HWND hWnd;
+    MSG msg;
+
+    hInst = hInstance;
+
+    // 윈도우 클래스 등록
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = nullptr;
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wcex)) {
+        MessageBox(nullptr,
+            L"Call to RegisterClassEx failed!",
+            L"Win32 Guided Tour",
+            NULL);
+
+        return 1;
+    }
+
+    // 윈도우 생성
+    hWnd = CreateWindow(
+        szWindowClass,
+        szTitle,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 300,
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr
+    );
+
+    if (!hWnd) {
+        MessageBox(nullptr,
+            L"Call to CreateWindow failed!",
+            L"Win32 Guided Tour",
+            NULL);
+
+        return 1;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    // 메시지 루프
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+// 윈도우 프로시저 함수
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+    switch (message) {
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+        DrawTriangle(hdc);
+        DrawRectangle(hdc);
+        DrawCircle(hdc);
+        EndPaint(hWnd, &ps);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 삼각형 그리기 함수
+void DrawTriangle(HDC hdc) {
+    POINT vertices[] = { {150, 50}, {100, 200}, {200, 200} };
+    Polygon(hdc, vertices, 3);
+}
+
+// 사각형 그리기 함수
+void DrawRectangle(HDC hdc) {
+    Rectangle(hdc, 250, 50, 350, 200);
+}
+
+// 원 그리기 함수
+void DrawCircle(HDC hdc) {
+    Ellipse(hdc, 400, 50, 500, 200);
+}
